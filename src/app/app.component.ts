c2d7c05123b6fb1ae6be9d7ec495d96f63677d11
dev_clemeno <dev.francesco.clemeno@outlook.it>
2022-04-05 22:49:37 +0200

    feature-deploy: add structure sidenav-toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnDestroy, ViewChild, HostListener } from '@angular/core';
+import {MediaMatcher} from '@angular/cdk/layout'
+import { MatSidenav } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'porfolio';
+export class AppComponent implements OnDestroy {
+
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
+  mobileQuery: MediaQueryList;
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    // Detect mobile device
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(): void {
+    this.sidenav.close();
+  }
+
 }
