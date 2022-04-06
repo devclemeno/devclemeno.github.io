e15682cac4dfd646130f5495a0b6450ad17f025a
dev_clemeno <dev.francesco.clemeno@outlook.it>
2022-04-06 22:00:54 +0200

    feature: complete home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
 
-  constructor() { }
+  intervalJob: any;
+  jobs = ['web developer', 'ux designer', 'software engineer', 'samurai'];
+  currentJobIndex = 0;
+
+  constructor(private router: Router) {
+    this.intervalJob = setInterval(() => {
+      this.currentJobIndex = this.currentJobIndex + 1;
+      if (this.currentJobIndex === this.jobs.length) {
+        this.currentJobIndex = 0;
+      }
+    }, 3000);
+  }
+
+  ngOnDestroy(): void {
+      clearInterval(this.intervalJob);
+  }
+
+  goAboutMe(): void {
+    this.router.navigate(['about-me']).then();
+  }
 
 }
