0b44278884f7d157a7cbe2534f7f7c6dfe94dfbc
dev_clemeno <dev.francesco.clemeno@outlook.it>
2022-04-07 21:45:30 +0200

    feature: improve about me and start contact me

--- src/app/components/about-me/about-me.component.ts
+++ src/app/components/about-me/about-me.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-me',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  goContactMe(): void {
+    this.router.navigate(['contact-me']).then();
+  }
 
 }
