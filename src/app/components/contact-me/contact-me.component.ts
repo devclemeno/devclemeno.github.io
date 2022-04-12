c4ceee56c232bdd6b26ab4c97ea67b609d32677b
dev_clemeno <dev.francesco.clemeno@outlook.it>
2022-04-12 21:18:45 +0200

    feature: add contact me view

--- src/app/components/contact-me/contact-me.component.ts
+++ src/app/components/contact-me/contact-me.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-contact-me',
   templateUrl: './contact-me.component.html',
   styleUrls: ['./contact-me.component.scss']
 })
-export class ContactMeComponent implements OnInit {
+export class ContactMeComponent {
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) { }
 
-  ngOnInit(): void {
+  onLinkedinClick(): void {
+    window.open('https://www.linkedin.com/in/francesco-clemeno', '_blank');
+  }
+
+  onInstagramClick(): void {
+    window.open('https://www.instagram.com/therealciccioclemeno', '_blank');
+  }
+
+  onFacebookClick(): void {
+    window.open('https://www.facebook.com/ciccio.clemeno', '_blank');
+  }
+
+  onYoutubeClick(): void {
+    this.snackBar.open('Coming Soon', 'OK', { duration: 1500 });
+  }
+
+  onTwitchClick(): void {
+    this.snackBar.open('Coming Soon', 'OK', { duration: 1500 });
   }
 
 }
